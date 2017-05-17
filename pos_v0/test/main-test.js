b80f3c2b37b7bf2014bfcb04c7701994a9bfcedb
jiaomei <1051415770@qq.com>
2017-05-17 17:08:10 +0800

    修改js题目

--- pos_v0/test/main-test.js
+++ pos_v0/test/main-test.js
@@ -9,50 +9,22 @@ describe('pos', () => {
         barcode: 'ITEM000000',
         name: '可口可乐',
         unit: '瓶',
-        price: 3.00
-
-      },
-      {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-      },
-      {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-      },
-      {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
-      },
-      {
-        barcode: 'ITEM000000',
-        name: '可口可乐',
-        unit: '瓶',
-        price: 3.00
+        price: 3.00,
+        count: 5
       },
       {
         barcode: 'ITEM000001',
         name: '雪碧',
         unit: '瓶',
-        price: 3.00
-      },
-      {
-        barcode: 'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: 3.00
+        price: 3.00,
+        count: 2
       },
       {
         barcode: 'ITEM000004',
         name: '电池',
         unit: '个',
-        price: 2.00
+        price: 2.00,
+        count: 1
       }
     ];
 
